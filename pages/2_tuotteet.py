54d08c8c43f5aa55755d3fb8e17032dabc18eb37
Kimmo <kimmolaine1@kamk.fi>
2025-05-16 00:03:17 +0300

    tuote sivua päivitetty

--- pages/2_tuotteet.py
+++ pages/2_tuotteet.py
@@ -59,8 +59,9 @@ st.markdown(
     | Sähköt 230V | tähän voi kytkeä esimerkiksi pöytävalaisimia, koristevaloja, muutamia läppäreitä tai puhelimen latauspistettä yms, ei mitään isompaa. |
     | Sähköt 1x16A 230V 3000W | 8 konepaikkaa. Sama kuin edellä (käytännössä edellinen on sama kuin tämä) mutta tähän ehkä vähän enemmän valaistusta, tai max 6-8 pöytäkonetta mutta ei muuta. tai esimerkiksi YKSI kahvinkeitin ja pieni jääkaappi. |
     | Sähköt 3x16A 400V 9000W | 24 konepaikkaa. Tähän voi sitten kytkeä jo enempi koneita, tehokkaampia valokannuja, kevyttä keittiövarustusta tai 3D tulostimia yms |
-    | Sähköt 3x32A 400V 15000W | 48 konepaikkaa. Tää alkaa olemaan jo aika paljon mutta tähän voi kytkeä jo oikeesti vähän enempi kamaa. Yliopiston standi on ollut muistaakseni ainoa jolle on tarvinut jotain näin tymäkkää tuoda kun siellä oli kaapillinen 3D tulostimia ja laserleikkuria yms./n
-      Tämä on myös se määrä tai yksikkö jonka esimerkiksi livelava ja sen tekniikka syö sähköä eli saanee vähän osviittaa että tuo on aika paljon sähköä. |
+    | Sähköt 3x32A 400V 15000W | 48 konepaikkaa. Tää alkaa olemaan jo aika paljon mutta tähän voi kytkeä jo oikeesti vähän enempi kamaa. 
+    Yliopiston standi on ollut muistaakseni ainoa jolle on tarvinut jotain näin tymäkkää tuoda kun siellä oli kaapillinen 3D tulostimia ja laserleikkuria yms 
+    Tämä on myös se määrä tai yksikkö jonka esimerkiksi livelava ja sen tekniikka syö sähköä eli saanee vähän osviittaa että tuo on aika paljon sähköä. |
     | verkko-1G Base-T |  |
     | verkko-10G SR |  |
     | verkko-10G LR |  |
